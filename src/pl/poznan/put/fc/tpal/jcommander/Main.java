59ae1f6b6aac8272005e8fca5858ceb1bbe6b784
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-04-14 15:05:33 +0200

    Added internationalization support

--- src/pl/poznan/put/fc/tpal/jcommander/Main.java
+++ src/pl/poznan/put/fc/tpal/jcommander/Main.java
@@ -5,14 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.poznan.put.fc.tpal.jcommander.controller.RootController;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/RootLayout.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/RootLayout.fxml"));
+        Locale locale = Locale.getDefault();
+        ResourceBundle bundle = ResourceBundle.getBundle("strings", locale);
+        loader.setResources(bundle);
+        Parent root = loader.load();
+
         primaryStage.setTitle("JCommander");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root));
+
+        RootController controller = loader.getController();
+        controller.setupData();
+
         primaryStage.show();
     }
 
