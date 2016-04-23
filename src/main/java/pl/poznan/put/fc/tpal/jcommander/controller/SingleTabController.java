32b58a48a3a302a7b1d71f5345895929fbe74684
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-04-23 15:43:35 +0200

    Fixed .gitignore

--- src/main/java/pl/poznan/put/fc/tpal/jcommander/controller/SingleTabController.java
+++ src/main/java/pl/poznan/put/fc/tpal/jcommander/controller/SingleTabController.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
